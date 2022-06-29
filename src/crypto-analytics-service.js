e8613a4f3cc217f3952a2ef9eab5dd4bbc439167
zbl14 <ifthereisoneday@gmail.com>
2022-06-29 14:50:53 -0700

    give two parameter for the funtion
    Co-authored-by: Daniel Ware <waredanielb@gmail.com>

--- src/crypto-analytics-service.js
+++ src/crypto-analytics-service.js
@@ -1,6 +1,6 @@
 export default class CryptoService {
-  static getCrypto(ticker) {
-    return fetch(`https://api.nomics.com/v1/currencies/ticker?key=${process.env.API_KEY}&ids=${ticker}`)
+  static getCrypto(ticker1, ticker2) {
+    return fetch(`https://api.nomics.com/v1/currencies/ticker?key=${process.env.API_KEY}&ids=${ticker1},${ticker2}`)
       .then(function(response){
         if (!response.ok) {
           throw Error(response.statusText);
