7c444b4ea3945b221ef652cc2ff069d14b5159cb
zbl14 <ifthereisoneday@gmail.com>
2022-06-29 15:37:15 -0700

    refactor w/ async
    Co-authored-by Daniel Ware <waredanielb@gmail.com>

--- src/crypto-analytics-service.js
+++ src/crypto-analytics-service.js
@@ -1,14 +1,13 @@
 export default class CryptoService {
-  static getCrypto(ticker1, ticker2) {
-    return fetch(`https://api.nomics.com/v1/currencies/ticker?key=${process.env.API_KEY}&ids=${ticker1},${ticker2}`)
-      .then(function(response){
+  static async getCrypto(ticker1, ticker2) {
+    try {
+    const response = await fetch(`https://api.nomics.com/v1/currencies/ticker?key=${process.env.API_KEY}&ids=${ticker1},${ticker2}`);
         if (!response.ok) {
           throw Error(response.statusText);
         }
         return response.json();
-      })
-      .catch(function(error) {
-        return error;
-      });
+      } catch(error) {
+        return error.message;
+      }
   }
 }
--- src/index.js
+++ src/index.js
@@ -10,50 +10,55 @@ function getTicker(response) {
     $('.showPrice').text(`The price for ${response[0].name} is ${response[0].price} at ${new Date(response[0].price_timestamp)}.`);
     $('.showVolume').text(`The trade volume for ${response[0].name} is ${response[0]["1d"].volume}.`);
   } else {
-    $('.showErrors').text(`There was an error processing your request: ${response.message}`);
+    $('.showErrors').text(`There was an error processing your request: ${response}`);
   }
+}
+
+async function makeApiCall(ticker1, ticker2) {
+  const response = await CryptoService.getCrypto(ticker1, ticker2);
+  getTicker(response);
 }
 
 function getTopCrypto(response) {
   if (response) {
     let tableOfTopCrypto = "";
     for (let i = 0; i < response.length; i++) {
-      tableOfTopCrypto += `<tr> <td>${response[i].rank}</td> <td>${response[i].name}</td> <td>${new Intl.NumberFormat('en-US').format(response[i].price)}</td> <td>${response[i]["1d"].volume}</td> </tr>`;
+      tableOfTopCrypto += `<tr> <td>${response[i].rank}</td> <td>${response[i].symbol}</td> <td>${response[i].name}</td> <td>${new Intl.NumberFormat('en-US').format(response[i].price)}</td> <td>${response[i]["1d"].volume}</td> </tr>`;
     }
     $("#topCrypto").before(tableOfTopCrypto);
   } else {
-    $('.showErrors').text(`There was an error processing your request: ${response.message}`);
+    $('.showErrors').text(`There was an error processing your request: ${response}`);
   }
+}
+
+async function makeApiCallForTable (ticker1, ticker2) {
+  const response = await CryptoService.getCrypto(ticker1, ticker2);
+  getTopCrypto(response);
 }
 
 function cryptConverter(response) {
   if (response) {
     let quantBuy = $('#quantityBuy').val();
-    console.log(quantBuy);
     let quantSell = quantBuy * response[0].price / response[1].price;
     $('.showCost').text(`The cost for ${quantBuy} of ${response[0].name} is ${ quantBuy * parseFloat(response[0].price)}`);
     $('#quantitySell').val(quantSell);
   }
 }
 
+async function makeApiCallForQuote (ticker1, ticker2) {
+  const response = await CryptoService.getCrypto(ticker1, ticker2);
+  cryptConverter(response);
+}
+
 $(document).ready(function(){
-  CryptoService.getCrypto("","")
-    .then(function(response) {
-      getTopCrypto(response);
-    });
+  makeApiCallForTable("","");
   $('#submit').click(function(){
-    let ticker = $('#name').val();
-    CryptoService.getCrypto(ticker, "")
-      .then(function(response) {
-        getTicker(response);
-      });
+    let ticker1 = $('#name').val();
+    makeApiCall(ticker1);
   });
   $('#quote').click(function(){
     let ticker1 = $('#buy').val();
     let ticker2 = $('#sell').val();
-    CryptoService.getCrypto(ticker1, ticker2)
-      .then(function(response) {
-        cryptConverter(response);
-      });
+    makeApiCallForQuote(ticker1, ticker2);
   });
 });
