14645b2929ab42f81d8ded2c2f6eec48ddda5426
zbl14 <ifthereisoneday@gmail.com>
2022-06-29 15:38:56 -0700

    fix identation

--- src/crypto-analytics-service.js
+++ src/crypto-analytics-service.js
@@ -1,13 +1,13 @@
 export default class CryptoService {
   static async getCrypto(ticker1, ticker2) {
     try {
-    const response = await fetch(`https://api.nomics.com/v1/currencies/ticker?key=${process.env.API_KEY}&ids=${ticker1},${ticker2}`);
-        if (!response.ok) {
-          throw Error(response.statusText);
-        }
-        return response.json();
-      } catch(error) {
-        return error.message;
+      const response = await fetch(`https://api.nomics.com/v1/currencies/ticker?key=${process.env.API_KEY}&ids=${ticker1},${ticker2}`);
+      if (!response.ok) {
+        throw Error(response.statusText);
       }
+      return response.json();
+    } catch(error) {
+      return error.message;
+    }
   }
 }
