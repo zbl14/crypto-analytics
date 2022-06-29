a58530411cd49c95eeaf692399c813baaaf1dcdc
zbl14 <ifthereisoneday@gmail.com>
2022-06-29 11:59:55 -0700

    have correct identation and fix missing semicolon

--- src/crypto-analytics-service.js
+++ src/crypto-analytics-service.js
@@ -1,14 +1,14 @@
 export default class CryptoService {
   static getCrypto(ticker) {
-    return fetch(`https://api.nomics.com/v1/currencies/ticker?key=${process.env.API_KEY}`)
-    .then(function(response){
-      if (!response.ok) {
-        throw Error(response.statusText);
-      }
-      return response.json();
-    })
-    .catch(function(error) {
-      return error;
-    })
+    return fetch(`https://api.nomics.com/v1/currencies/ticker?key=${process.env.API_KEY}&ids=${ticker}`)
+      .then(function(response){
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function(error) {
+        return error;
+      });
   }
 }
